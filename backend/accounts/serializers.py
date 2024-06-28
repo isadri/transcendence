629c59a905d32615938592213fe6d7267ad0c141
Issam Abkadri <iabkadri@student.1337.ma>
2024-06-28 11:57:59 +0100

    add password confirmation field

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -7,9 +7,13 @@ class UserSerializer(serializers.ModelSerializer):
 	password = serializers.CharField(write_only=True, required=True, style={
 		'input_type': 'password'
 	})
+	password_confirmation = serializers.CharField(write_only=True, required=True, style={
+		'input_type': 'password'
+	})
 
 	class Meta:
 		model = models.User
 		fields = [
-			'username', 'email', 'first_name', 'last_name', 'password'
+				'username', 'email', 'first_name', 'last_name', 'password',
+				'password_confirmation'
 			]
