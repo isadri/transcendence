8f60dec1eea63ec64d2a250dac5363d32340c498
hex01e <houmanso@student.1337.ma>
2024-08-27 22:18:45 +0100

    setup main css vars (colors/fonts)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import './App.css'
 function App() {
   return (
     <>
+    <div >Hamza</div>
+    <div style={{fontFamily:'Josefin Sans'}}>Hamza</div>
+    <div style={{fontFamily:'JetBrains Mono'}}>Hamza</div>
     </>
   )
 }
