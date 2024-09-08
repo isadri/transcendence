b7c766afdd07d460d4e57a607db3f376e334ca72
naimaelbaz <nnaimaelbaz@gmail.com>
2024-09-08 10:00:30 +0100

    merge with chat brunch

--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -2,8 +2,17 @@ import './Home.css'
 
 function Home() {
   return (
-    <div>
-      
+    <div className='homePage'>
+      <div className='searchBar'>
+        <form action="">
+        <i className="fa-solid fa-magnifying-glass"></i>
+          <input type="search" placeholder="Search..." />
+        </form>
+        <div className='Icons'>
+          <i className="fa-solid fa-bell"></i>
+          <i className="fa-solid fa-user-plus"></i>
+        </div>
+      </div>
     </div>
   )
 }
