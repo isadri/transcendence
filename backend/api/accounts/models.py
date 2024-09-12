95ee8196f629fa6be10e4ea9ae18a1fbe1ee5ef0
Issam Abkadri <iabkadri@student.1337.ma>
2024-09-12 18:13:17 +0100

    add docstring

--- backend/api/accounts/models.py
+++ backend/api/accounts/models.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    avatar = models.ImageField(upload_to='profiles',
+    """
+    Custom User model.
+    """
+    avatar = models.ImageField(_('Avatar'), upload_to='avatars', blank=True,
+                                null=True,
                                 default='default/default_profile.jpg')
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Return username as a string represention of the object.
+        """
         return self.username
