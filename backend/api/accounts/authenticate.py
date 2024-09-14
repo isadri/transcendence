c96cacf4578b8ccc288f0a3e2f2ea4b8dd08e895
Issam Abkadri <iabkadri@student.1337.ma>
2024-09-14 16:10:20 +0100

    styling

--- backend/api/accounts/authenticate.py
+++ backend/api/accounts/authenticate.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.request import Request
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import Token
+
+from .models import User
 
 
 class TokenAuthentication(JWTAuthentication):
@@ -11,14 +15,22 @@ class TokenAuthentication(JWTAuthentication):
     JWT tokens.
     """
 
-    def authenticate(self, request):
+    def authenticate(self, request: Request) -> tuple[User, Token] | None:
         """
-        Verify if the request contains a valid JWT token
+        Verify if the request contains a valid JWT token.
 
         This functions checks if the request contains a JWT token stored in
-        the cookies, and if this token is found, and it is valid, the function
-        returns the validated token and the user using the given validated
-        token.
+        the cookies. If it is not stored in the cookies, this function will
+        try to get the token from the request header. If the token is found,
+        and is valid, the function returns the validated token and the user
+        using the given validated token.
+
+        Raises:
+            AuthenticationFailed: If the authentication is attempted but fails.
+
+        Returns:
+            Two-tuple of (user, token), None if the token is not found or the
+            token is not valid or no such user exists with the validated token.
         """
         header = self.get_header(request)
         if header:
