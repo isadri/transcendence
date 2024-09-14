8167679b13fabd5c5a97474de6421d10f69b80ed
Issam Abkadri <iabkadri@student.1337.ma>
2024-09-14 12:09:13 +0100

    create custom user model and custom user manager

--- backend/api/accounts/admin.py
+++ backend/api/accounts/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User
 
 
-admin.site.register(User)
+class UserAdmin(BaseUserAdmin):
+    #list_display = ('username', 'email', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal info', {'fields': ('first_name',)}),
+        ('Permissions', {'fields': ('is_staff',)}),
+    )
+
+
+admin.site.register(User, UserAdmin)
--- backend/api/accounts/models.py
+++ backend/api/accounts/models.py
@@ -1,19 +1,147 @@
-from django.contrib.auth.models import AbstractUser
+import logging
+from typing import Optional
+from django.apps import apps
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.validators import ASCIIUsernameValidator
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
-class User(AbstractUser):
-    """
-    Custom User model.
-    """
-    email = models.EmailField(_('Email'), unique=True)
-    avatar = models.ImageField(_('Avatar'), upload_to='avatars', blank=True,
-                                null=True,
-                                default='default/default_profile.jpg')
+logger = logging.getLogger(__name__)
 
-    def __str__(self) -> str:
+
+class UserManager(BaseUserManager):
+    """
+    Custom user manager.
+    """
+    def create_user(self, username: str, email: str,
+                    password: Optional[str]=None, **extra_fields) -> 'User':
         """
-        Return username as a string representation of the object.
+        Create and save a user with the given username, email, password and
+        extra_fields.
+
+        Raises:
+            ValueError: If the username or the email were not given.
+
+        Returns:
+            A new user.
         """
-        return self.username
+        if not username:
+            raise ValueError('The username must be given')
+        if not email:
+            raise ValueError('The email must be given')
+        logger.debug(email)
+        email = self.normalize_email(email)
+        logger.debug(email)
+
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
+        )
+        username = GlobalUserModel.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username: str, email: str,
+                    password: Optional[str]=None, **extra_fields) -> 'User':
+        """
+        Create a superuser.
+
+        Returns:
+            A new superuser.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_active=True.')
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class User(PermissionsMixin, AbstractBaseUser):
+    """
+    Custom user model.
+    """
+    username_validators = ASCIIUsernameValidator()
+
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        validators=[username_validators],
+        help_text=_('Required. 150 characters or fewer. Letters, digits, and '
+                    '@/./+/-/_ only.'),
+        error_messages={
+            'unique': _('A user with that username already exists.'),
+        }
+    )
+    first_name = models.CharField(_('first name'), max_length=150, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+        error_messages={
+            'unique': _('A user with that email address already exists.')
+        }
+    )
+    avatar = models.ImageField(
+        _('avatar'),
+        help_text=_('The profile picture'),
+        upload_to='avatars',
+        default='default.jpg'
+    )
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_(
+            'Designates whether the user can log into this admin site.'
+        )
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user model should be treated as active.'
+            ' Unselect this instead of deleting accounts.'
+        )
+    )
+    date_joined = models.DateTimeField(_('date joined'),
+                                       default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
+
+    def clean(self) -> None:
+        """
+        Normalize the username and the email.
+        """
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self) -> str:
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = f'{first_name} {last_name}'
+        return full_name
+
+    def get_short_name(self) -> str:
+        """
+        Return the short name for the user.
+        """
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs) -> None:
+        """
+        Send an email to this user.
+        """
+        send_email(subject, message, from_email, [self.email], **kwargs)
