5297c4fbace2c719a1eaa51079cc02e874367e6b
Issam Abkadri <iabkadri@student.1337.ma>
2024-09-15 15:08:09 +0100

    add Friends model

--- backend/api/friends/models.py
+++ backend/api/friends/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from . import friends
+
+
+class Friends(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE, related_name='user')
+    friends = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True,
+                                     related_name='friends')
+
+    def __str__(self) -> str:
+        return self.user.username
+
+    def add(self, new_friend: 'User') -> None:
+        if new_friend in self.friends.all():
+            raise friends.AlreadyExistsError
+        self.friends.add(new_friend)
