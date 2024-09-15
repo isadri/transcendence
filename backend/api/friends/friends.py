810fe9deed8a9ed19827f9e8f30ea09f0155bcac
Issam Abkadri <iabkadri@student.1337.ma>
2024-09-15 15:03:02 +0100

    define root exception

---
+++ backend/api/friends/friends.py
@@ -0,0 +1,10 @@
+class Error(Exception):
+    """
+    Base-class for all exceptions raised by this module.
+    """
+
+
+class AlreadyExistsError:
+    """
+    This object is already exist.
+    """
