f577124e8a539da4492bc089d724952e9900877c
Issam Abkadri <iabkadri@student.1337.ma>
2024-09-16 12:41:34 +0100

    send and accept friend requests

--- backend/api/friends/urls.py
+++ backend/api/friends/urls.py
@@ -8,6 +8,9 @@ app_name = 'friends'
 
 
 urlpatterns = [
+    path('send/', views.FriendRequestSendView.as_view(), name='send'),
+    path('accept/', views.FriendRequestAcceptView.as_view(),
+        name='accept'),
     path('add/', views.FriendAddView.as_view(), name='new'),
     #path('<str:username>/detail/', views.FriendDetailView.as_view(),
     #     name='detail'),
--- backend/api/friends/views.py
+++ backend/api/friends/views.py
@@ -9,7 +9,7 @@ from rest_framework.request import Request
 from rest_framework.views import APIView
 
 from . import friends
-from .models import Friend
+from .models import Friend, FriendRequest
 from .serializers import FriendSerializer
 from .utils import get_object
 from ..accounts.serializers import UserSerializer
@@ -18,6 +18,77 @@ from ..accounts.serializers import UserSerializer
 logger = logging.getLogger(__name__)
 
 User = get_user_model()
+
+
+class FriendRequestSendView(APIView):
+    """
+    This view is used to send friend requests.
+    """
+
+    def post(self, request: Request) -> Response:
+        """
+        Send a friend request.
+        """
+        try:
+            receiver_username = request.data.get('username', '')
+            receiver = User.objects.get(username=receiver_username)
+        except User.DoesNotExist:
+            return Response({'error': 'User does not exist.'},
+                            status=status.HTTP_404_NOT_FOUND)
+        friend_request, created = (
+            FriendRequest.objects.get_or_create(sender=request.user,
+                                                receiver=receiver))
+        if created:
+            return Response({'message': 'the request is sent.'},
+                            status=status.HTTP_201_CREATED)
+        else:
+            return Response({'error': 'the request is already sent.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class FriendRequestAcceptView(APIView):
+    """
+    This view is used to accept friend requests.
+
+    """
+
+    def add_friends(self, sender: User, receiver: User) -> None:
+        """
+        Add sender to the list of friends of receiver, and vice versa.
+        """
+        sender_friend_list = Friend.objects.get(user=sender)
+        receiver_friend_list = Friend.objects.get(user=receiver)
+        sender_friend_list.add(receiver)
+        receiver_friend_list.add(sender)
+
+    def post(self, request: Request) -> Response:
+        """
+        Accept the friend request.
+
+        Returns:
+            HTTP_404_NOT_FOUND response: If there is no friend request to the
+                current user.
+            HTTP_400_BAD_REQUEST response: If the current user and the sender
+                user are already friends.
+            HTTP_200_OK response: If the current user accept the friend request
+                of the sender user.
+        """
+        sender_username = request.data.get('sender_username', '')
+        sender = User.objects.get(username=sender_username)
+        receiver = request.user
+        try:
+            friend_request = FriendRequest.objects.get(sender=sender,
+                                                       receiver=request.user)
+            self.add_friends(sender, receiver)
+        except FriendRequest.DoesNotExist:
+            return Response({'error': 'No such request'},
+                            status=status.HTTP_404_NOT_FOUND)
+        except friends.AlreadyExistsError:
+            return Response({'error': 'This user is already a friend.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': 'friend request accepted'},
+                        status=status.HTTP_200_OK)
 
 
 class FriendAddView(APIView):
