f3d9a0a29790ffcc91388b78a83aa41dfd4e05e2
ylachhab <yassminelachhabtata@gmail.com>
2024-09-18 19:43:02 +0100

    add menu in chat

--- frontend/src/Pages/Chat/components/ChatCenter.tsx
+++ frontend/src/Pages/Chat/components/ChatCenter.tsx
@@ -3,45 +3,42 @@ import "./ChatCenter.css";
 import { Message } from "./types";
 
 interface ChatCenterProps {
-  messages: Message[];
+	messages: Message[];
 }
 
 const ChatCenter = ({ messages }: ChatCenterProps) => {
-  const endRef = useRef<HTMLDivElement>(null);
+	const endRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    endRef.current?.scrollIntoView({ behavior: "instant" });
-  }, [messages]);
+	useEffect(() => {
+		endRef.current?.scrollIntoView({ behavior: "instant" });
+	}, [messages]);
 
-  return (
-    <div className="center">
-      {messages.map((value, index) => {
-        const isOwnMessage = value.senderId === 2;
-        return (
-          <div
-            key={index}
-            className={isOwnMessage ? "message-own" : "message"}
-          >
-            {!isOwnMessage && (
-              <img src={value.profile} alt="profile" className="profile" />
-            )}
-            <div className="textMessage">
-              {value.image && (
-                <img
-                  src={value.image}
-                  alt="imgPartage"
-                  className="imgPartage"
-                />
-              )}
-              <p>{value.message}</p>
-              <span>{value.time}</span>
-            </div>
-          </div>
-        );
-      })}
-      <div ref={endRef}></div>
-    </div>
-  );
+	return (
+		<div className="center">
+			{messages.map((value, index) => {
+				const isOwnMessage = value.senderId === 2;
+				return (
+					<div key={index} className={isOwnMessage ? "message-own" : "message"}>
+						{!isOwnMessage && (
+							<img src={value.profile} alt="profile" className="profile" />
+						)}
+						<div className="textMessage">
+							{value.image && (
+								<img
+									src={value.image}
+									alt="imgPartage"
+									className="imgPartage"
+								/>
+							)}
+							<p>{value.message}</p>
+							<span>{value.time}</span>
+						</div>
+					</div>
+				);
+			})}
+			<div ref={endRef}></div>
+		</div>
+	);
 };
 
 export default ChatCenter;
--- frontend/src/Pages/Chat/components/ChatTop.tsx
+++ frontend/src/Pages/Chat/components/ChatTop.tsx
@@ -1,23 +1,37 @@
 import { Friend } from "./types";
 import "./ChatTop.css";
+import { useState } from "react";
 
 interface ChatTopProps {
-  selectedFriend: Friend;
+	selectedFriend: Friend;
 }
 
 const ChatTop = ({ selectedFriend }: ChatTopProps) => {
-  return (
-    <div className="top">
-      <div className="profileInfo">
-        <img src={selectedFriend.profile} alt="profile" className="image" />
-        <div className="textInfo">
-          <span>{selectedFriend.name}</span>
-          <p>Last seen today 00:56</p>
-        </div>
-      </div>
-      <i className="fa-solid fa-ellipsis-vertical icon"></i>
-    </div>
-  );
+	const [openMenu, setOpenMenu] = useState(false);
+
+	return (
+		<div className="top">
+			<div className="profileInfo">
+				<img src={selectedFriend.profile} alt="profile" className="image" />
+				<div className="textInfo">
+					<span>{selectedFriend.name}</span>
+					<p>Last seen today 00:56</p>
+				</div>
+			</div>
+			<i
+				className={`fa-solid fa-ellipsis-vertical icon-menu ${openMenu ? "activeMenu" : ""}`}
+				onClick={() => setOpenMenu(!openMenu)}
+			></i>
+			{openMenu && (
+				<ul className="menu-list">
+					<li>Invite to play</li>
+					<li>Delete chat</li>
+					<li>Close chat</li>
+					<li>Block</li>
+				</ul>
+			)}
+		</div>
+	);
 };
 
 export default ChatTop;
