4f04bb4fd84154f1a3520f9980189fe6a028b57a
naimaelbaz <nnaimaelbaz@gmail.com>
2024-09-18 11:46:46 +0100

    add badges list to profile

--- frontend/src/Pages/Profile/Components/BadgesList.tsx
+++ frontend/src/Pages/Profile/Components/BadgesList.tsx
@@ -5,24 +5,33 @@ import bg3 from '../images/badge3.svg'
 import bg4 from '../images/badge4.svg'
 
 function BadgesList() {
-  return (
+
+  const ScrollLeft = ()=>{
+      var left = document.querySelector(".badgesList")
+      left?.scrollBy(-125,0);
+  }
+  const ScrollRight = ()=>{
+      var right = document.querySelector(".badgesList")
+      right?.scrollBy(125,0);
+  }
+
+return (
     <div className='BadgesList'>
       <h2>Badges List</h2>
       <div className='badgesScroll'>
         <div className='chevron-left'>
-        <i className="fa-solid fa-chevron-left"></i>
+        <button onClick={ScrollLeft}><i className="fa-solid fa-chevron-left"></i></button>
         </div>
         <div className='badgesList'>
-          <img src={bg2} alt="" />
-          <img src={bg4} alt="" />
-          <img src={bg2} alt="" />
-          <img src={bg3} alt="" />
-          <img src={bg5} alt="" />
-          <img src={bg5} alt="" />
-          <img src={bg5} alt="" />
+          <div className='child'><img src={bg2} alt="" /></div>
+          <div className='child'><img src={bg4} alt="" /></div>
+          <div className='child'><img src={bg2} alt="" /></div>
+          <div className='child'><img src={bg3} alt="" /></div>
+          <div className='child'><img src={bg5} alt="" /></div>
+          <div className='child'><img src={bg5} alt="" /></div>
         </div>
         <div className='chevron-right'>
-        <i className="fa-solid fa-chevron-right"></i>
+        <button onClick={ScrollRight}><i className="fa-solid fa-chevron-right"></i></button>
         </div>
       </div>
     </div>
