0cb370b112f0d9448a709fde68114106f7d3b0c9
Issam Abkadri <iabkadri@student.1337.ma>
2024-10-03 11:55:58 +0100

    create frontend app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import LoginForm from "./components/LoginForm/LoginForm";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
