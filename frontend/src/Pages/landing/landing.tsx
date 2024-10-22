11157d06b1bded3bdcbbbb9566fe80fcf34a520e
hex01e <houmanso@student.1337.ma>
2024-10-22 15:37:47 +0100

    Get started landing ppgae 3d

--- frontend/src/Pages/landing/landing.tsx
+++ frontend/src/Pages/landing/landing.tsx
@@ -1,20 +1,62 @@
+import { Canvas, ThreeElements, useFrame } from '@react-three/fiber'
 import './landing.css'
-import NavBar from '../../components/FirstNavBar/navBar'
+import { OrbitControls, useGLTF } from '@react-three/drei'
 
-function landing() {
+
+function Model(){
+  const url = new URL("./../../assets/sceen_without_balls.glb", import.meta.url);
+  const nodes = useGLTF(url.pathname)
+
+  return (
+    <group>
+      <primitive object={nodes}/>
+    </group>
+  )
+}
+
+function LandingSceen() {
+
+
+  // const tableGltf = useRef<Scene>(null!);
+
+  // useFrame((state, delta)=>{
+  //   tableGltf.current.rotation.y += delta*0.75;
+  // })
+
   return (
     <>
-        <div className='landing'>
-          <div className='landingCon'>
-            <h1>welcome to our game </h1>
-            <div className='paragraph'>
-                <p>Are you ready to show off your ping pong skills? Play now and experience the ultimate table tennis game with realistic physics, thrilling gameplay, and endless competition!</p>
-            </div>
-            <button>Join Us</button>
-          </div>
-        </div>
+      <OrbitControls />
+      <perspectiveCamera/>
+      <directionalLight position={[0 , 5, 0]} intensity={2}/>
+      <Model/>
+      {/* <Gltf src={(new URL("./../../assets/sceen_without_balls.glb", import.meta.url)).pathname} ref={tableGltf}/> */}
     </>
   )
 }
 
-export default landing
+
+function Landing() {
+  return (
+    <>
+      <div className='landing'>
+        <div className='landing3d'>
+          <Canvas camera={{position: [0, 3, 7]}}>
+            <LandingSceen/>
+          </Canvas>
+        </div>
+        {/* <div className='landingCon'>
+          <h1>welcome to our game </h1>
+          <div className='paragraph'>
+            <p>Are you ready to show off your ping pong skills? Play now and experience the ultimate table tennis game with realistic physics, thrilling gameplay, and endless competition!</p>
+          </div>
+          <div className="buttons">
+            <button className="btn" id="loginBtn">Join Us</button>
+          </div>
+        </div> */}
+        
+      </div>
+    </>
+  )
+}
+
+export default Landing
