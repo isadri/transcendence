e54b3c87cea31d39b25b3e55ea4cf8b371e2f42a
hex01e <houmanso@student.1337.ma>
2024-11-18 14:16:21 +0100

    move the racke and the ball

--- frontend/src/Pages/Game/Play/Play.tsx
+++ frontend/src/Pages/Game/Play/Play.tsx
@@ -1,4 +1,4 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, Vector3 } from "@react-three/fiber";
 import "./Play.css";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import {
@@ -9,52 +9,59 @@ import {
   useSphere,
 } from "@react-three/cannon";
 import { Material } from 'cannon-es';
+import { useEffect } from "react";
+import { AxesHelper } from "three";
 
 const tableUrl = new URL("../images/pongTable.glb", import.meta.url).href;
 useGLTF.preload(tableUrl);
 
 function Ball() {
-  const ballMaterial = new Material();
-  ballMaterial.name = "ball_mat"
-  const [ball_ref, ball_api] = useSphere(() => ({
+  const material = new Material();
+  material.name = "ball_mat"
+  const [ref, api] = useSphere(() => ({
     mass: 0.1,
-    position: [0, 2, 0],
-    args: [0.07],
-    material:ballMaterial
-    // material: {
-    //   restitution: 0.9, // Makes the ball bouncy
-    // },
+    position: [0, 0.2, 0],
+    args: [0.06],
+    material:material
   }));
+  
+
+  api.velocity.set(0, 0, 9);
+  useEffect(() => {
+    const reposition = api.position.subscribe(([x, y, z]) => {
+      if (y < -3)
+      {
+        api.position.set(0, 0.2, 0);
+        api.velocity.set(0, 0, 9);
+      }
+
+    })
+  }, [api])
 
   return (
-    <mesh ref={ball_ref}>
-      <sphereGeometry args={[0.07, 30, 30]} />
+    <mesh ref={ref}>
+      <sphereGeometry args={[0.06, 30, 30]} />
       <meshStandardMaterial />
     </mesh>
   );
 }
 
 function Table() {
-  const tableMaterial = new Material();
-  tableMaterial.name = "table_mat"
+  const material = new Material();
+  material.name = "table_mat"
   const table = useGLTF(tableUrl);
-  const [table_ref, table_api] = useBox(() => ({
+  const [ref, api] = useBox(() => ({
     position: [0, 0, 0],
     type: "Static",
     args: [1.9038, 0.0364, 3.629],
-    material: tableMaterial
+    material: material
   }));
+
 
   return (
     <>
-      {/*
-      <mesh ref={table_ref} position={[0, -0.0351, 0]}>
-        <boxGeometry args={[1.9038, 0.07, 3.629]} />
-      </mesh>
-      */}
-
       <primitive
-        ref={table_ref}
+        ref={ref}
         object={table.scene}
         position={[0, 0, 0]}
       />
@@ -62,17 +69,80 @@ function Table() {
   );
 }
 
-function GameTable() {
 
+interface RacketProps {
+  position: any, // can be  Vector3 or Triplet,
+  mine?:boolean
+}
+function Racket({position, mine=false}: RacketProps){
+  const material = new Material();
+  material.name = "racket_mat"
+  const [ref, api] = useBox(() => ({
+    mass: 10,
+    type:"Static",
+    position: position,
+    args: [0.5, 0.15, 0.2],
+    material: material
+  }));
+
+  const onKeyDown = (event: KeyboardEvent) => {
+    if (mine){
+      api.position.subscribe(([x, y, z]) =>{
+        if  (event.key == "ArrowRight")
+          api.position.set(x + 0.02, y, z)
+        if  (event.key == "ArrowLeft")
+          api.position.set(x - 0.02, y, z)
+      })
+    }
+  }
+
+  const onKeyUp = (event: KeyboardEvent) => {
+    api.position.subscribe(([x, y, z]) =>{
+      api.position.set(x, y, z);
+    })
+  }
+  useEffect(() => {
+    window.addEventListener("keydown", onKeyDown)
+    window.addEventListener("keyup", onKeyUp)
+
+    return () => {
+      window.removeEventListener("keydown", onKeyDown);
+      window.removeEventListener("keyup", onKeyUp);
+    };
+  }, [api, onKeyDown, onKeyUp, mine])
+
+  return (
+    <mesh ref={ref} position={position}>
+      <boxGeometry args={[0.5, 0.15, 0.2]} />
+      <meshStandardMaterial/>
+    </mesh>
+  )
+}
+
+function GameTable() {
   // contact  beetween the ball and the table 
   useContactMaterial("ball_mat", "table_mat", {
-    restitution: 0.8,
+    friction:0,
+    restitution: 0,
+  });
+  // contact  beetween the racket and the table 
+  useContactMaterial("racket_mat", "table_mat", {
+    friction:0.9,
+    restitution: 0,
+  });
+  useContactMaterial("racket_mat", "ball_mat", {
+    friction:0,
+    restitution: 1,
   });
 
   return (
     <>
       <Ball />
       <Table />
+      <Racket position={[0, 0.09, +1.6]} mine/>
+      <Racket position={[0, 0.09, -1.6]}/>
+      <primitive object={new AxesHelper(5)} />
+
     </>
   );
 }
@@ -82,7 +152,8 @@ const Play = () => {
     <Canvas camera={{ position: [0, 2, 5] }}>
       <OrbitControls />
       <perspectiveCamera />
-      <directionalLight position={[-33, 9, 5]} intensity={2} />
+      <directionalLight position={[-50, 9, 5]} intensity={1} />
+      <directionalLight position={[-50, 9, -5]} intensity={1} />
       <directionalLight position={[3, 9, 5]} intensity={2} />
       <Physics gravity={[0, -9.81, 0]}>
         <Debug>
