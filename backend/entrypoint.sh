bdb3863de9cd6cce36d951b7163421b04821376e
isadri <issam.abk01@gmail.com>
2024-11-25 12:40:27 +0100

    create superuser only if does not exist

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,5 +4,18 @@ echo "Running database migrations"
 python manage.py makemigrations accounts
 python manage.py makemigrations friends
 python manage.py migrate
-python manage.py createsuperuser --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --noinput
+
+cat << EOF | python manage.py shell
+from django.contrib.auth import get_user_model
+import os
+
+username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+email = os.getenv('DJANGO_SUPERUSER_EMAIL')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+
+if not get_user_model().objects.filter(username=username).exists():
+	get_user_model().objects.create_superuser(username, email, password)
+
+EOF
+
 exec "$@" 
