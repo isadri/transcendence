e84c875045f854fffcda04acfb1c56cee74606cb
isadri <issam.abk01@gmail.com>
2024-11-27 14:05:53 +0100

    validate password

--- backend/api/accounts/serializers.py
+++ backend/api/accounts/serializers.py
@@ -1,4 +1,4 @@
-import re
+import string
 from rest_framework import serializers
 
 from .models import User
@@ -29,7 +29,26 @@ class UserSerializer(serializers.ModelSerializer):
         if len(value) < 8:
             raise serializers.ValidationError('password too short. '
                                               'At least 8 characters.')
-        regexp = re.compile('^([a-z]+|[A-Z]+|[0-9]){8,}')
+        upper_case = 0
+        lower_case = 0
+        digits = 0
+        specials = 0
+        for c in value:
+            if c.isupper():
+                upper_case += 1
+            if c.islower():
+                lower_case += 1
+            if c.isdigit():
+                digits += 1
+            if c in string.punctuation:
+                specials += 1
+        if not upper_case or not lower_case or not digits or not specials:
+            raise serializers.ValidationError('password must contain at least '
+                                              'one lowercase character, '
+                                              'one uppercase character, '
+                                              'one digit, and one special '
+                                              'symbol.')
+        return value
 
     def create(self, validated_data: dict[str, str]) -> User:
         """
--- r.py
+++ r.py
@@ -3,7 +3,7 @@ import sys
 
 
 if __name__ == '__main__':
-    regexp = re.compile('(?=ok)')
+    regexp = re.compile('([a-z]+([A-Z]+[0-9]+|[0-9]+[A-Z]+))')
     if regexp.search(sys.argv[1]):
         print('OK')
     else:
