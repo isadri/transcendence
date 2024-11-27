f8c5bffc12a4438012d1cd2ba66409fb90680950
isadri <issam.abk01@gmail.com>
2024-11-27 15:23:10 +0100

    validate username in the serializer

--- backend/api/accounts/serializers.py
+++ backend/api/accounts/serializers.py
@@ -19,6 +19,28 @@ class UserSerializer(serializers.ModelSerializer):
             'username', 'email', 'password', 'avatar'
             ]
 
+    def validate_username(self, value: str) -> str:
+        """
+        Validate the username.
+        
+        Raises:
+            serializers.ValidationError: If the username is not valid.
+        """
+        if User.objects.filter(username__iexact=value).exists():
+            raise serializers.ValidationError('A user with this username '
+                                             'already exists.')
+        if not value[0] in string.ascii_lowercase and value[0] != '_':
+            raise serializers.ValidationError('The username must begin with '
+                                             ' a lowercase character or a _.')
+        if len(value) < 3 or len(value) > 15:
+            raise serializers.ValidationError('username must at least contain 3'
+                                ' and at most 15 characters.')
+        allowed_characters = string.ascii_lowercase + string.digits + '_' + '-' + '.'
+        if any(c for c in value if c not in allowed_characters):
+            raise serializers.ValidationError("The username can only contain "
+                                "lowercase characters, digits, '.', '_', or '-'.")
+        return value
+
     def validate_password(self, value: str) -> str:
         """
         Validate the password.
