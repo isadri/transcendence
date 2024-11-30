83c1a3f94e9306408d8eee9d56b38fce823614fe
ylachhab <yassminelachhabtata@gmail.com>
2024-11-30 15:51:32 +0100

    add fuction of format time in models

--- backend/api/chat/models.py
+++ backend/api/chat/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.db.models import UniqueConstraint
+from django.utils.timesince import timesince
 
 class Chat(models.Model):
     user1 = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='chats_as_user1',
@@ -28,6 +29,18 @@ class Message(models.Model):
     file = models.FileField(upload_to='chat_files/%y/%m/%d', null=True, blank=True)
     image = models.ImageField(upload_to='chat_files/%y/%m/%d', null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def formatted_close_time(self):
+        # HH:MM AM/PM
+        return self.created_at.strftime("%I:%M %p")
+
+    def formatted_far_time(self):
+        # November 17, 2024
+        return self.created_at.strftime("%B %d, %Y")
+
+    def human_readable_time(self):
+        # 5 minutes ago
+        return timesince(self.timestamp)
 
     def __str__(self):
         return f"Message from {self.sender.username} in chat {self.chat.id}"
