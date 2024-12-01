f628143bfa827d49d0d4f70043bf75e4ac833122
hex01e <houmanso@student.1337.ma>
2024-12-01 22:46:44 +0100

    start a random game (create an model obj)

--- backend/api/game/consumers.py
+++ backend/api/game/consumers.py
@@ -1,10 +1,31 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import Game
 
 
 class GameStart(AsyncWebsocketConsumer):
 
+  qeuee = {}
   async def connect(self):
+    self.user = self.scope["user"]
+    self.qeuee[self.user.username] = self.user
+    if len(self.qeuee) >= 2:
+      iterator = iter(iter(self.qeuee.items()))
+      key1 , player1 = next(iterator)
+      key2 , player2 = next(iterator)
+      print(player1 , "vs", player2)
+      gameMatch = await self.create_game(player1, player2)
+      print(gameMatch)
+      del self.qeuee[key1]
+      del self.qeuee[key2]
+      print(self.qeuee)
     await self.accept()
+
 
   async def disconnect(self, code):
     print(code)
+
+
+  @database_sync_to_async
+  def create_game(self, player1, player2):
+    return Game.objects.create(player1=player1, player2=player2)
--- backend/api/game/models.py
+++ backend/api/game/models.py
@@ -12,7 +12,7 @@ GAME_STATE = [
 class Game(models.Model):
   player1  = models.ForeignKey(User, on_delete=models.CASCADE, related_name="player1")
   player2  = models.ForeignKey(User, on_delete=models.CASCADE, related_name="player2")
-  progress = models.CharField(max_length=1, choices=GAME_STATE)
+  progress = models.CharField(max_length=1, choices=GAME_STATE, default="S")
   start_at = models.DateTimeField(auto_now_add=True)
   p1_score = models.SmallIntegerField(default=0)
   p2_score = models.SmallIntegerField(default=0)
