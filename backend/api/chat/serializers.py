80884d600f19638b6438faa4c0be24b4d274f0cd
ylachhab <yassminelachhabtata@gmail.com>
2024-12-01 14:36:45 +0100

    add user2 in serializer

--- backend/api/chat/serializers.py
+++ backend/api/chat/serializers.py
@@ -18,7 +18,7 @@ class MessageSerializer(serializers.ModelSerializer):
 
 class ChatSerializer(serializers.ModelSerializer):
     messages = MessageSerializer(many=True, read_only=True)
-    # user2 = UserSerializer() ######### add this after end of chat modification #########
+    user2 = UserSerializer() ######### add this after end of chat modification #########
 
     class Meta:
         model = Chat
