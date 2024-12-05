7a2e5edb69fee13bca237e990f72c39e0baa7e8e
isadri <issam.abk01@gmail.com>
2024-12-05 17:19:37 +0100

    use custom formatter

---
+++ backend/config/formatters.py
@@ -0,0 +1,18 @@
+import logging
+import json_log_formatter
+from django.utils import timezone
+
+
+class CustomizedJSONFormatter(json_log_formatter.JSONFormatter):
+    def json_record(self, message: str, extra: dict, record: logging.LogRecord) -> dict:
+        extra['message'] = message
+        extra['level'] = record.levelname
+        extra['name'] = record.name
+        
+        if 'user' not in extra:
+            extra['user'] = 'Anonymous User'
+        if 'time' not in extra:
+            extra['time'] = timezone.now()
+        if record.exc_info:
+            extra['exc_info'] = self.formatException(record.exc_info)
+        return extra
