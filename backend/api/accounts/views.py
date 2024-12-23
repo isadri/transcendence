bb5c954d82814ce26bc5660db9b3ec907827d632
isadri <issam.abk01@gmail.com>
2024-12-23 22:44:19 +0100

    download intra picture and set it as avatar for user

--- backend/api/accounts/views.py
+++ backend/api/accounts/views.py
@@ -1,5 +1,7 @@
+import io
 import os
 import pyotp
+import requests
 from typing import Optional
 from django.conf import settings
 from django.contrib.auth import (
@@ -9,6 +11,7 @@ from django.contrib.auth import (
 )
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.core.files.base import File
 from django.shortcuts import (
     get_object_or_404,
     redirect,
@@ -298,6 +301,19 @@ class IntraLoginViewSet(viewsets.ViewSet):
     permission_classes = [AllowAny]
     authentication_classes = []
 
+    def set_avatar(self, user: User, avatar_url: str) -> None:
+        """
+        Get the avatar if available and set it to the user.
+        """
+        response = requests.get(avatar_url)
+        if response.status_code == 200:
+            image_content = io.BytesIO(response.content)
+            user.avatar.save(
+                os.path.basename(avatar_url),
+                File(image_content)
+            )
+            user.save()
+
     def list(self, request: Request) -> Response:
         """
         Authenticate with the authorization server and obtain user information.
@@ -309,6 +325,7 @@ class IntraLoginViewSet(viewsets.ViewSet):
         if status_code != 200:
             return Response(user_info, status=status_code)
         user = get_user(user_info, 'intra')
+        self.set_avatar(user, user_info['image']['link'])
         if user.otp_active:
             generate_otp_for_user(user)
             send_otp_email(user)
