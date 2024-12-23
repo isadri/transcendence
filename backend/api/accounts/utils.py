c3afe95d9698fc49ee56ac5e798c0da8d1a2a5ee
isadri <issam.abk01@gmail.com>
2024-12-23 19:37:27 +0100

    move send emails functions to mails.py file

--- backend/api/accounts/utils.py
+++ backend/api/accounts/utils.py
@@ -311,58 +311,6 @@ def usernamePolicyWrong(value: str):
     return False
 
 
-def send_email_verification(user: User, confirmation_url: str) -> None:
-    """
-    Send the verification email to the user.
-    """
-    html_message = f"""
-    <html>
-        <head>
-            <style>
-                body {{
-                    font-family: Arial, sans-serif;
-                    background-color: #000;
-                    color: #333;
-                    margin: 0;
-                    padding: 0;
-                }}
-                .email-container {{
-                    background-color: #ffffff;
-                    border: 1px solid #ddd;
-                    padding: 20px;
-                    margin: 20px;
-                    border-radius: 8px;
-                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-                }}
-                h1 {{
-                    color: #4CAF50;
-                }}
-                p {{
-                    font-size: 16px;
-                }}
-            </style>
-        </head>
-        <body>
-            <div class="email-container">
-                <h1>Please confirm your Email</h1>
-                <p>Click here to confirm your email:</p>
-                <a href="{confirmation_url}"
-                style="background-color: #4CAF50; color: white; padding: 10px 20px; border-radius: 5px; text-decoration: none; font-weight: bold; display: inline-block;">
-                    Confirm
-                </a>
-            </div>
-        </body>
-    </html>
-    """
-    user.email_user(
-        subject='Please confirm your Email',
-        message=('Click this link to confirm your email '
-                 f'{confirmation_url}'),
-        from_email=settings.EMAIL_HOST_USER,
-        html_message=html_message
-    )
-
-
 def validate_token(username: str, token: str) -> User | None:
     """
     Return the user that has the given token. Return
