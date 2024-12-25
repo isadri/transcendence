176c5cfb272128d5f31467259d320fd300a168fc
hex01e <houmanso@student.1337.ma>
2024-12-25 13:42:55 +0100

    fix entrypoint script

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -9,14 +9,14 @@ import os
 from django.contrib.auth import get_user_model
 
 
-username = os.gentenv('DJANGO_SUPERUSER_USERNAME')
-email = os.gentenv('DJANGO_SUPERUSER_EMAIL')
-password = os.getenv('DJANGO_SUPERUSER_EMAIL')
+username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+email = os.getenv('DJANGO_SUPERUSER_EMAIL')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
 
 if not get_user_model().objects.filter(username=username).exists():
     get_user_model().objects.create_superuser(username, email, password)
 
 EOF
 
-python manage.py 0:8000
+python manage.py runserver 0:8000
 
