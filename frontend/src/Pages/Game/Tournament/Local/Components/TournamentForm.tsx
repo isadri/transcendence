4bc3866d32293149a09e32f01077f5d5008dc07e
hex01e <houmanso@student.1337.ma>
2024-12-29 21:42:08 +0100

    Tournament Local:shuffl players

--- frontend/src/Pages/Game/Tournament/Local/Components/TournamentForm.tsx
+++ frontend/src/Pages/Game/Tournament/Local/Components/TournamentForm.tsx
@@ -1,38 +1,98 @@
+import { useState } from "react";
+import Game from "../../../Game";
 import "./TournamentForm.css";
 
-interface Props {
+interface GameFormProps {
 	players: string[];
 }
 
-const TournamentForm = ({players}: Props) => {
+interface TournamentPlayer {
+	alias: string,
+	score: number,
+}
+
+interface TournamentGameData {
+	player1: TournamentPlayer,
+	player2: TournamentPlayer,
+	winner: TournamentPlayer | null
+}
+
+interface TournamentData {
+	half1: TournamentGameData,
+	half2: TournamentGameData,
+	final: TournamentGameData | null,
+	winner: TournamentPlayer | null
+}
+
+interface TournamentGraghProps {
+	data: TournamentData;
+	setData: React.Dispatch<React.SetStateAction<TournamentData>>;
+}
+
+
+
+const TournamentGragh = ({ data, setData }: TournamentGraghProps) => {
 	return (
 		<div className="tournament-list">
 			<h2>Tournament Local</h2>
 			<div className="tournament-players">
 				<div className="first-two-match">
 					<div className="tournament-match">
-						<div>{players[0]}</div>
+						<div>{data.half1.player1.alias}</div>
 						<img src="/../Group.png" alt="" />
-						<div>{players[1]}</div>
+						<div>{data.half1.player2.alias}</div>
 					</div>
 					<div className="tournament-match">
-						<div>{players[2]}</div>
+						<div>{data.half2.player1.alias}</div>
 						<img src="/../Group.png" alt="" />
-						<div>{players[3]}</div>
+						<div>{data.half2.player2.alias}</div>
 					</div>
 				</div>
 				<div className="tournament-match">
-					<div>{players[0]}</div>
+					<div>{data.final ? data.final.player1.alias : '-'}</div>
 					<img src="/../Group.png" alt="" />
-					<div>{players[3]}</div>
+					<div>{data.final ? data.final.player2.alias : '-'}</div>
 				</div>
 				<div className="tournament-match">
-					<div>{players[0]}</div>
+					<div>{data.winner ? data.winner.alias : '-'}</div>
 				</div>
 			</div>
 			<button className="start-btn">start</button>
 		</div>
 	);
 };
+
+function TournamentForm({ players }: GameFormProps) {
+	const shuffled: string[] = players.sort(() => Math.random() - 0.5);
+	const [data, setData] = useState<TournamentData>({
+		half1: {
+			player1: { alias: shuffled[0], score: 0 },
+			player2: { alias: shuffled[1], score: 0 },
+			winner: null
+		},
+		half2: {
+			player1: { alias: shuffled[2], score: 0 },
+			player2: { alias: shuffled[3], score: 0 },
+			winner: null
+		},
+	 final: null,
+	 winner: null
+	})
+
+	console.log(players);
+
+
+	console.log(shuffled);
+
+	return (
+		<>
+			{true ?
+				<TournamentGragh data={data} setData={setData} players={players} />
+				:
+				<Game />
+			}
+		</>
+	)
+}
 
 export default TournamentForm;
