a2740c076080a78c6efdb14843ff8f51403a2ac1
hex01e <houmanso@student.1337.ma>
2024-12-31 09:48:09 +0100

    tournament local logic done

--- frontend/src/Pages/Game/Tournament/Local/Components/TournamentForm.tsx
+++ frontend/src/Pages/Game/Tournament/Local/Components/TournamentForm.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import Game from "../../../Game";
+import { useEffect, useRef, useState } from "react";
 import "./TournamentForm.css";
+import Play from "../../../Play/Play";
 
 interface GameFormProps {
 	players: string[];
@@ -26,12 +26,13 @@ export interface TournamentData {
 
 interface TournamentGraghProps {
 	data: TournamentData;
+	setPlay: React.Dispatch<React.SetStateAction<boolean>>;
 	setData: React.Dispatch<React.SetStateAction<TournamentData>>;
 }
 
 
 
-const TournamentGragh = ({ data, setData }: TournamentGraghProps) => {
+const TournamentGragh = ({ data, setData, setPlay}: TournamentGraghProps) => {
 	return (
 		<div className="tournament-list">
 			<h2>Tournament Local</h2>
@@ -57,15 +58,15 @@ const TournamentGragh = ({ data, setData }: TournamentGraghProps) => {
 					<div>{data.winner ? data.winner.alias : '-'}</div>
 				</div>
 			</div>
-			<button className="start-btn">start</button>
+			<button className="start-btn" onClick={() => { setPlay(true) }}>start</button>
 		</div>
 	);
 };
 
-function shufflePlayers(players:string[]) {
+function shufflePlayers(players: string[]) {
 	for (let i = players.length - 1; i > 0; i--) {
-			const randomIndex = Math.floor(Math.random() * (i + 1));
-			[players[i], players[randomIndex]] = [players[randomIndex], players[i]];
+		const randomIndex = Math.floor(Math.random() * (i + 1));
+		[players[i], players[randomIndex]] = [players[randomIndex], players[i]];
 	}
 	return players;
 }
@@ -73,6 +74,7 @@ function shufflePlayers(players:string[]) {
 
 function TournamentForm({ players }: GameFormProps) {
 	const shuffled: string[] = shufflePlayers(players)
+	const [play, setPlay] = useState<boolean>(false)
 	const [data, setData] = useState<TournamentData>({
 		half1: {
 			player1: { alias: shuffled[0], score: 0 },
@@ -84,21 +86,39 @@ function TournamentForm({ players }: GameFormProps) {
 			player2: { alias: shuffled[3], score: 0 },
 			winner: null
 		},
-	 final: null,
-	 winner: null
+		final: null,
+		winner: null
 	})
+	const [game, setGame] = useState<TournamentGameData>(data.half1)
+	
 
-	console.log(players);
+	console.log("hello: ", data)
+	useEffect(() => {
+		if (!game) return
+		if (!data.half2.winner && game.winner)
+			setGame(data.half2)
+		else if (data.half1.winner && data.half2.winner)
+		{
+			const newgame:TournamentGameData = {
+				player1: { alias: data.half1.winner.alias, score: 0 },
+				player2: { alias: data.half2.winner.alias, score: 0 },
+				winner: null
+			}
+			setGame(newgame)
+			setData({...data, final : newgame})
+		}
+		if (data.final && data.final.winner)
+			setData({...data, winner: data.final.winner})
 
+	}, [game?.winner])
 
-	console.log(shuffled);
 
 	return (
 		<>
-			{true ?
-				<TournamentGragh data={data} setData={setData} players={players} />
+			{!play ?
+				<TournamentGragh data={data} setData={setData} setPlay={setPlay} />
 				:
-				<Game />
+				<Play switcher={setPlay} game={game} setGame={setGame} />
 			}
 		</>
 	)
