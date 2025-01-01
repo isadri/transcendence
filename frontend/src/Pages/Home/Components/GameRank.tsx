1e42aabbfecc8f9419d69909c58410235ffe010e
naimaelbaz <nnaimaelbaz@gmail.com>
2025-01-01 15:50:04 +0100

    update NoStats components to indicate no games or rankings

--- frontend/src/Pages/Home/Components/GameRank.tsx
+++ frontend/src/Pages/Home/Components/GameRank.tsx
@@ -1,139 +1,71 @@
 import "../styles/GameRank.css";
 import photo from "../images/profile.svg";
 import bg1 from "../images/badge1.svg";
+import { useState } from "react";
 
 function GameRank() {
+  const [there_is_rank, setThere_is_rank] = useState(false)
   return (
     <div className="Home-GameRank">
-      {/* <h2>Game Rank</h2> */}
-      {/* <div className="Home-gameRankEle"> */}
-        <div className="Home-RowEle">
-          <div className="Home-row1">
-            <span>1</span>
-            <img src={bg1} alt="" />
-            <div className="Home-ProfileRev">
-              <img src={photo} alt="" />
-              <span>User1dfgdfgdfg</span>
+      {
+        there_is_rank &&
+        <>
+          <div className="Home-RowEle">
+            <div className="Home-row1">
+              <span>1</span>
+              <img src={bg1} alt="" />
+              <div className="Home-ProfileRev">
+                <img src={photo} alt="" />
+                <span>User1dfgdfgdfg</span>
+              </div>
+            </div>
+            <div className="Home-row2">
+              <span>1452 xp</span>
+              <span>5.22 lvl</span>
             </div>
           </div>
-          <div className="Home-row2">
-            <span>1452 xp</span>
-            <span>5.22 lvl</span>
-          </div>
-        </div>
-        <div className="Home-RowEle">
-          <div className="Home-row1">
-            <span>1</span>
-            <img src={bg1} alt="" />
-            <div className="Home-ProfileRev">
-              <img src={photo} alt="" />
-              <span>User1dfgdfgdfg</span>
+          <div className="Home-RowEle">
+            <div className="Home-row1">
+              <span>1</span>
+              <img src={bg1} alt="" />
+              <div className="Home-ProfileRev">
+                <img src={photo} alt="" />
+                <span>User1dfgdfgdfg</span>
+              </div>
+            </div>
+            <div className="Home-row2">
+              <span>1452 xp</span>
+              <span>5.22 lvl</span>
             </div>
           </div>
-          <div className="Home-row2">
-            <span>1452 xp</span>
-            <span>5.22 lvl</span>
-          </div>
-        </div>
-        <div className="Home-RowEle">
-          <div className="Home-row1">
-            <span>1</span>
-            <img src={bg1} alt="" />
-            <div className="Home-ProfileRev">
-              <img src={photo} alt="" />
-              <span>User1dfgdfgdfg</span>
+          <div className="Home-RowEle">
+            <div className="Home-row1">
+              <span>1</span>
+              <img src={bg1} alt="" />
+              <div className="Home-ProfileRev">
+                <img src={photo} alt="" />
+                <span>User1dfgdfgdfg</span>
+              </div>
+            </div>
+            <div className="Home-row2">
+              <span>1452 xp</span>
+              <span>5.22 lvl</span>
             </div>
           </div>
-          <div className="Home-row2">
-            <span>1452 xp</span>
-            <span>5.22 lvl</span>
-          </div>
-        </div>
-        <div className="Home-RowEle">
-          <div className="Home-row1">
-            <span>1</span>
-            <img src={bg1} alt="" />
-            <div className="Home-ProfileRev">
-              <img src={photo} alt="" />
-              <span>User1dfgdfgdfg</span>
+        </>
+      }
+      {
+        !there_is_rank &&
+        <div className='Nostats game-NoStats'>
+            <div className='stats-icon'>
+            <i className="fa-solid fa-ranking-star"></i>
+            </div>
+            <div className='NoStats-msg'>
+              <h3>No rankings available</h3>
+              <span>No games have been played yet across all users</span>
             </div>
           </div>
-          <div className="Home-row2">
-            <span>1452 xp</span>
-            <span>5.22 lvl</span>
-          </div>
-        </div>
-        <div className="Home-RowEle">
-          <div className="Home-row1">
-            <span>1</span>
-            <img src={bg1} alt="" />
-            <div className="Home-ProfileRev">
-              <img src={photo} alt="" />
-              <span>User1dfgdfgdfg</span>
-            </div>
-          </div>
-          <div className="Home-row2">
-            <span>1452 xp</span>
-            <span>5.22 lvl</span>
-          </div>
-        </div>
-        <div className="Home-RowEle">
-          <div className="Home-row1">
-            <span>1</span>
-            <img src={bg1} alt="" />
-            <div className="Home-ProfileRev">
-              <img src={photo} alt="" />
-              <span>User1dfgdfgdfg</span>
-            </div>
-          </div>
-          <div className="Home-row2">
-            <span>1452 xp</span>
-            <span>5.22 lvl</span>
-          </div>
-        </div>
-        <div className="Home-RowEle">
-          <div className="Home-row1">
-            <span>1</span>
-            <img src={bg1} alt="" />
-            <div className="Home-ProfileRev">
-              <img src={photo} alt="" />
-              <span>User1dfgdfgdfg</span>
-            </div>
-          </div>
-          <div className="Home-row2">
-            <span>1452 xp</span>
-            <span>5.22 lvl</span>
-          </div>
-        </div>
-        <div className="Home-RowEle">
-          <div className="Home-row1">
-            <span>1</span>
-            <img src={bg1} alt="" />
-            <div className="Home-ProfileRev">
-              <img src={photo} alt="" />
-              <span>User1dfgdfgdfg</span>
-            </div>
-          </div>
-          <div className="Home-row2">
-            <span>1452 xp</span>
-            <span>5.22 lvl</span>
-          </div>
-        </div>
-        <div className="Home-RowEle">
-          <div className="Home-row1">
-            <span>1</span>
-            <img src={bg1} alt="" />
-            <div className="Home-ProfileRev">
-              <img src={photo} alt="" />
-              <span>User1dfgdfgdfg</span>
-            </div>
-          </div>
-          <div className="Home-row2">
-            <span>1452 xp</span>
-            <span>5.22 lvl</span>
-          </div>
-        </div>
-      {/* </div> */}
+      }
     </div>
   );
 }
--- frontend/src/Pages/Home/Components/LastGame.tsx
+++ frontend/src/Pages/Home/Components/LastGame.tsx
@@ -1,97 +1,119 @@
-import '../styles/LastGame.css'
-import photo from '../images/profile.svg'
-import Group from '../images/Group.svg'
+import "../styles/LastGame.css";
+import photo from "../images/profile.svg";
+import Group from "../images/Group.svg";
+import { useState } from "react";
 
 function LastGame() {
+  const [there_is_rank, setThere_is_rank] = useState(false) 
+
   return (
-    <div className='Home-LastGame'>
-      <div className='Home-RowEle'>
-        <div className='Home-Row1'>
-            <img src={photo} alt="" />
-            <span>user1kkkkkkkkkk</span>
-        </div>
-        <div>
-        <div className='Home-Row2'>
-          <span className='Home-score1'>1</span>
-          <img src={Group} alt="" />
-          <span className='Home-score2'>1</span>
-        </div>
-        </div>
-        <div className='Home-Row3'>
-            <span>user2hhhhhhhhhh</span>
-            <img src={photo} alt="" />
-        </div>
-      </div>
-      <div className='Home-RowEle'>
-        <div className='Home-Row1'>
-            <img src={photo} alt="" />
-            <span>user1</span>
-        </div>
-        <div>
-        <div className='Home-Row2'>
-          <span className='Home-score1'>1</span>
-          <img src={Group} alt="" />
-          <span className='Home-score2'>1</span>
-        </div>
-        </div>
-        <div className='Home-Row3'>
-            <span>user2</span>
-            <img src={photo} alt="" />
-        </div>
-      </div>
-      <div className='Home-RowEle'>
-        <div className='Home-Row1'>
-            <img src={photo} alt="" />
-            <span>user1</span>
-        </div>
-        <div>
-        <div className='Home-Row2'>
-          <span className='Home-score1'>1</span>
-          <img src={Group} alt="" />
-          <span className='Home-score2'>1</span>
-        </div>
-        </div>
-        <div className='Home-Row3'>
-            <span>user2</span>
-            <img src={photo} alt="" />
-        </div>
-      </div>
-      <div className='Home-RowEle'>
-        <div className='Home-Row1'>
-            <img src={photo} alt="" />
-            <span>user1</span>
-        </div>
-        <div>
-        <div className='Home-Row2'>
-          <span className='Home-score1'>1</span>
-          <img src={Group} alt="" />
-          <span className='Home-score2'>1</span>
-        </div>
-        </div>
-        <div className='Home-Row3'>
-            <span>user2</span>
-            <img src={photo} alt="" />
-        </div>
-      </div>
-      <div className='Home-RowEle'>
-        <div className='Home-Row1'>
-            <img src={photo} alt="" />
-            <span>user1</span>
-        </div>
-        <div>
-        <div className='Home-Row2'>
-          <span className='Home-score1'>1</span>
-          <img src={Group} alt="" />
-          <span className='Home-score2'>1</span>
-        </div>
-        </div>
-        <div className='Home-Row3'>
-            <span>user2</span>
-            <img src={photo} alt="" />
-        </div>
-      </div>
+    <div className="Home-LastGame">
+      {
+        there_is_rank &&
+        <>
+          <div className="Home-RowEle">
+            <div className="Home-Row1">
+              <img src={photo} alt="" />
+              <span>user1kkkkkkkkkk</span>
+            </div>
+            <div>
+              <div className="Home-Row2">
+                <span className="Home-score1">1</span>
+                <img src={Group} alt="" />
+                <span className="Home-score2">1</span>
+              </div>
+            </div>
+            <div className="Home-Row3">
+              <span>user2hhhhhhhhhh</span>
+              <img src={photo} alt="" />
+            </div>
+          </div>
+          <div className="Home-RowEle">
+            <div className="Home-Row1">
+              <img src={photo} alt="" />
+              <span>user1</span>
+            </div>
+            <div>
+              <div className="Home-Row2">
+                <span className="Home-score1">1</span>
+                <img src={Group} alt="" />
+                <span className="Home-score2">1</span>
+              </div>
+            </div>
+            <div className="Home-Row3">
+              <span>user2</span>
+              <img src={photo} alt="" />
+            </div>
+          </div>
+          <div className="Home-RowEle">
+            <div className="Home-Row1">
+              <img src={photo} alt="" />
+              <span>user1</span>
+            </div>
+            <div>
+              <div className="Home-Row2">
+                <span className="Home-score1">1</span>
+                <img src={Group} alt="" />
+                <span className="Home-score2">1</span>
+              </div>
+            </div>
+            <div className="Home-Row3">
+              <span>user2</span>
+              <img src={photo} alt="" />
+            </div>
+          </div>
+          <div className="Home-RowEle">
+            <div className="Home-Row1">
+              <img src={photo} alt="" />
+              <span>user1</span>
+            </div>
+            <div>
+              <div className="Home-Row2">
+                <span className="Home-score1">1</span>
+                <img src={Group} alt="" />
+                <span className="Home-score2">1</span>
+              </div>
+            </div>
+            <div className="Home-Row3">
+              <span>user2</span>
+              <img src={photo} alt="" />
+            </div>
+          </div>
+          <div className="Home-RowEle">
+            <div className="Home-Row1">
+              <img src={photo} alt="" />
+              <span>user1</span>
+            </div>
+            <div>
+              <div className="Home-Row2">
+                <span className="Home-score1">1</span>
+                <img src={Group} alt="" />
+                <span className="Home-score2">1</span>
+              </div>
+            </div>
+            <div className="Home-Row3">
+              <span>user2</span>
+              <img src={photo} alt="" />
+            </div>
+          </div>
+        </>
+      }
+      {
+         !there_is_rank &&
+         <div className='Nostats game-NoStats'>
+             <div className='stats-icon'>
+             <i className="fa-solid fa-table-tennis-paddle-ball"></i>
+             </div>
+             <div className='NoStats-msg'>
+               <h3>No games played yet</h3>
+               <span>You haven't played any games so far.
+                Once you start playing,
+                your game history will appear here!</span>
+             </div>
+           </div>
+      }
     </div>
-  )
+  );
 }
 
-export default LastGame
+export default LastGame;
