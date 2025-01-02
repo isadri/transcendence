dff97792bec7775964f866d04e2988ec9f3ecde4
hex01e <houmanso@student.1337.ma>
2025-01-02 20:54:50 +0100

    append it to routing

--- backend/api/game/routing.py
+++ backend/api/game/routing.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .consumers import RandomGame, RemoteGame
+from .consumers import RandomGame, RemoteGame, RandomTournament
 
 websocket_urlpatterns = [
   path("ws/game/random/", RandomGame.as_asgi()),
+  path("ws/game/tournament/random", RandomTournament.as_asgi()),
   path("ws/game/remote/<int:game_id>", RemoteGame.as_asgi())
 ]
