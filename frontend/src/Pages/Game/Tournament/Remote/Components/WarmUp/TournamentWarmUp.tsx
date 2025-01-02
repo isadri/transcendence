99d2e68254f7282f9f8ddbc96905ea147ce9c7e9
hex01e <houmanso@student.1337.ma>
2025-01-02 20:30:43 +0100

    edit warmup for many enemies

--- frontend/src/Pages/Game/Tournament/Remote/Components/WarmUp/TournamentWarmUp.tsx
+++ frontend/src/Pages/Game/Tournament/Remote/Components/WarmUp/TournamentWarmUp.tsx
@@ -9,8 +9,11 @@ import { getUser, getendpoint } from "../../../../../../context/getContextData";
 
 interface PlayerCardData {
   enemy?: boolean,
+  enemyIndex?: number,
   isRandom?: boolean,
 }
+type EnemysUserData = [ EnemyUserData | null, EnemyUserData | null, EnemyUserData | null]
+const emptyEnemys:EnemysUserData = [null, null, null]
 
 interface EnemyUserData {
   username: string,
@@ -22,8 +25,8 @@ interface EnemyUserData {
 interface ContextData {
   socket: WebSocket | null,
   setSocket: React.Dispatch<React.SetStateAction<WebSocket | null>>,
-  enemyUser: EnemyUserData | null
-  setEnemyUser: React.Dispatch<React.SetStateAction<EnemyUserData | null>>,
+  enemys: EnemysUserData
+  setEnemys: React.Dispatch<React.SetStateAction<EnemysUserData>>,
   setDisplayFriends: React.Dispatch<React.SetStateAction<boolean>>,
   ready: boolean,
   setReady: React.Dispatch<React.SetStateAction<boolean>>
@@ -31,28 +34,28 @@ interface ContextData {
 
 export const WarmUpContext = createContext<ContextData | null>(null)
 
-const PlayerCard = ({ enemy = false, isRandom = false }: PlayerCardData) => {
+const PlayerCard = ({ enemy = false, isRandom = false, enemyIndex=0 }: PlayerCardData) => {
   const user = getUser()
   const context = useContext(WarmUpContext)
   const navigator = useNavigate()
   if (context) {
-    const { socket, setSocket, enemyUser, setEnemyUser, setReady, setDisplayFriends } = context
+    const { socket, setSocket, enemys, setEnemys, setReady, setDisplayFriends } = context
     useEffect(() => {
       if (socket)
         socket.onmessage = (e) => {
           const data = JSON.parse(e.data)
           console.log(data);
           if (data.event == "HANDSHAKING") {
-            setTimeout(() => { setEnemyUser(data.enemy) }, 2000);
+            setTimeout(() => { setEnemys([data.enemy, null, null]) }, 2000);
             setTimeout(() => { navigator(`/game/remote/${data.game_id}`) }, 5000);
           }
           if (data.event == "ABORT") {
-            setEnemyUser(null)
+            setEnemys(emptyEnemys)
             setReady(false)
             socket.close()
           }
         }
-    }, [setEnemyUser, setSocket, socket])
+    }, [setEnemys, setSocket, socket])
 
     const inviteFriend = () => {
       console.log('here');
@@ -63,7 +66,7 @@ const PlayerCard = ({ enemy = false, isRandom = false }: PlayerCardData) => {
       <div className="TournamentWarmUpVsPlayer" >
         <div className="TournamentWarmUpVsImageDiv">
           {
-            enemy && !enemyUser ?
+            enemy && !enemys[enemyIndex] ?
               <div className="TournamentWarmUpVsPlus" onClick={inviteFriend}>
                 <div>
                   <i className={`fa-solid ${isRandom ? "fa-hourglass-start" : "fa-plus"} fa-2xl`}></i>
@@ -72,9 +75,9 @@ const PlayerCard = ({ enemy = false, isRandom = false }: PlayerCardData) => {
               </div>
               :
               (
-                enemy && enemyUser ?
+                enemy && enemys[enemyIndex] ?
                   <>
-                    <img src={getendpoint("http", enemyUser.avatar)} className="TournamentWarmUpVsAvatar" />
+                    <img src={getendpoint("http",enemys[enemyIndex]?.avatar || "")} className="TournamentWarmUpVsAvatar" />
                     <img src={badge} className="TournamentWarmUpVsBadge" />
                   </>
                   :
@@ -91,9 +94,9 @@ const PlayerCard = ({ enemy = false, isRandom = false }: PlayerCardData) => {
             enemy ?
               <>
                 {
-                  enemyUser ?
+                  enemys[enemyIndex] ?
                     <>
-                      <h4>{enemyUser.username}</h4>
+                      <h4>{enemys[enemyIndex]?.username}</h4>
                       <h4>4.5 lvl</h4>
                     </>
                     :
@@ -129,17 +132,14 @@ const PlayerCard = ({ enemy = false, isRandom = false }: PlayerCardData) => {
 const ReadyContext = ({ isRandom = false }: PlayerCardData) => {
   const context = useContext(WarmUpContext)
   if (context) {
-    let { socket, ready, setReady, setSocket, setEnemyUser, enemyUser } = context
-    console.log(enemyUser);
+    let { socket, ready, setReady, setSocket, setEnemys, enemys } = context
+    console.log(enemys);
 
     const onReady = () => {
       if (ready)
         return
       console.log('isRandom', isRandom)
-      const newSocket = new WebSocket(isRandom ? 
-            getendpoint('ws', '/ws/game/random/'):
-            getendpoint('ws', '/ws/game/friend/')
-          )
+      const newSocket = new WebSocket(getendpoint('ws', '/ws/game/tournament/random'))
       setSocket(newSocket)
       newSocket.onopen = () => {
         
@@ -148,7 +148,10 @@ const ReadyContext = ({ isRandom = false }: PlayerCardData) => {
         }))
       }
       newSocket.onclose = () => {
-        setEnemyUser(null)
+        console.log('s: closed');
+        
+        setReady(false)
+        setEnemys([null, null, null])
       }
       setReady(true)
     }
@@ -177,13 +180,14 @@ const ReadyContext = ({ isRandom = false }: PlayerCardData) => {
   }
 }
 
+
+
 const TournamentWarmUp = ({ isRandom = false }: { isRandom?: boolean }) => {
-  const [displayFriends, setDisplayFriends] = useState<boolean>(false)
   let [socket, setSocket] = useState<WebSocket | null>(null)
   const [ready, setReady] = useState<boolean>(false)
-  const [enemyUser, setEnemyUser] = useState<EnemyUserData | null>(null)
+  const [enemys, setEnemys] = useState<EnemysUserData>(emptyEnemys)
   return (
-    <WarmUpContext.Provider value={{ socket, setSocket, enemyUser, setEnemyUser, ready, setReady, setDisplayFriends }}>
+    <WarmUpContext.Provider value={{ socket, setSocket, enemys, setEnemys, ready, setReady }}>
       <div className="TournamentGameWarmUp">
         <h2>Warm Up</h2>
         <div className="TournamentWarmUpOther">
