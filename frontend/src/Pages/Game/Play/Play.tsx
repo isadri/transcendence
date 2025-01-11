488c608e258282e28064c805689d0da38e01adaf
ylachhab <yassminelachhabtata@gmail.com>
2025-01-11 08:32:02 +0100

    update image in play

--- frontend/src/Pages/Game/Play/Play.tsx
+++ frontend/src/Pages/Game/Play/Play.tsx
@@ -2,7 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import "./Play.css";
 import winner from "../../../assets/winner.png"
 import vs from "../../Home/images/Group.svg"
-import pic from "../../Home/images/profile.svg"
+import pic from "../../Setting/images/default.jpeg"
 import "../../Home/styles/LastGame.css"
 
 import { OrbitControls, useGLTF } from "@react-three/drei";
